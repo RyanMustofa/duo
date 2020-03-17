45f4bd6bfd35132a0662224d48a233fba4a7d8e2
RyanMustofa <rm281003@gmail.com>
2020-03-18 06:51:56 +0700

    update

--- src/container/Dashboard.jsx
+++ src/container/Dashboard.jsx
@@ -13,33 +13,64 @@ class Dashboard extends Component {
         axios
             .get("https://al-quran-8d642.firebaseio.com/data.json?print=pretty")
             .then(res => {
-                console.log(res)
+                console.log(res);
                 this.setState({
                     dataSurat: res.data,
-                    loading: true
+                    loading: true,
+                    connection: true
+                });
+            })
+            .catch(res => {
+                this.setState({
+                    connection: false
                 });
             });
     }
-    handleClick = (nomor) => {
-        this.props.history.push(`/${nomor}`)
-    }
+    handleClick = nomor => {
+        this.props.history.push(`/${nomor}`);
+    };
     render() {
-        if(this.state.loading === false){
-            return <h1>LOAIDNG...</h1>
+        if (this.state.loading === false) {
+            return <h1>LOAIDNG...</h1>;
         }
-        return (
-            <div>
-                {this.state.dataSurat.map(data => {
-                    return (
-                        <table className="responsive-table">
-                            <tr>
-                                <td onClick={() => this.handleClick(data.nomor)}>{data.asma}</td>
-                            </tr>
-                        </table>
-                    );
-                })}
-            </div>
-        );
+        if(this.state.connection === false){
+            return <h1>CHECK YOUR INTERNET</h1>
+        }
+        if (this.state.connection) {
+            return (
+                <div>
+                    <table className="responsive-table">
+                        {this.state.dataSurat.map(data => {
+                            return (
+                                <tr>
+                                    <td
+                                        onClick={() =>
+                                            this.handleClick(data.nomor)
+                                        }
+                                    >
+                                        {data.asma}
+                                    </td>
+                                    <td
+                                        onClick={() =>
+                                            this.handleClick(data.nomor)
+                                        }
+                                    >
+                                        {data.nama}
+                                    </td>
+                                    <td
+                                        onClick={() =>
+                                            this.handleClick(data.nomor)
+                                        }
+                                    >
+                                        {data.arti}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </table>
+                </div>
+            );
+        }
     }
 }
 
